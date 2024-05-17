00ef4f82582c475d2ed05c936013fedf40a5c587
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-05-17 08:12:15 -0400

    Explored JS Object Constructors

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function Book(title, author, pageCount, read){
-    this.title = title;
-    this.author = author;
-    this.pageCount = pageCount;
-    this.read = read;
-    this.info = function(){
-        return `${this.title} by ${this.author}, ${pageCount} pages, ${this.read}`
-    };
-}
+// function Book(title, author, pageCount, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pageCount = pageCount;
+//     this.read = read;
+//     this.info = function(){
+//         return `${this.title} by ${this.author}, ${pageCount} pages, ${this.read}`
+//     };
+// }
 
 
 function Player(name, marker) {
